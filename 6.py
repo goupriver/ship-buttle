ba6835ade88331637e1efb79f29d331c71744ca7
Alexander Mironov <goupriver@gmail.com>
2024-01-26 22:36:44 +0700

    итог

--- 6.py
+++ 6.py
@@ -9,41 +9,40 @@ def coloredString(color, string):
     BLUE = "\033[0;34m"
     YELLOW = "\033[1;33m"
 
+    BLACK = "\u001b[40;1m"
+    DARK = "\u001b[47;1m"
+
     if color == 'BLUE':
         return BLUE + string + END
     if color == 'RED':
-        return RED + string + END
+        return  RED + string + END
     if color == 'GREEN':
-        return GREEN + string + END
+        return  GREEN + string + END
     if color == 'YELLOW':
         return YELLOW + string + END
+    if color == 'BLACK':
+        return BLACK + string + END
+    if color == 'DARK':
+        return DARK + string + END
 
 class Ship:
     def __init__(self, positions):
-        self.positions = positions  # ?? массив [(x,y), (x,y)] - позиции точек корабля
-        self.hits = []   # список - массив [(x, y), (x, y)] - список выстрелов по кораблю
+        self.positions = positions
+        self.hits = []
 
     def is_sunk(self):
         return len(self.hits) == len(self.positions)
 
 class Board:
     def __init__(self):
-        self.board = [['O' for _ in range(6)] for _ in range(6)] # [[0,0,T,0,X],[],[]]   # доска
-        self.ships = []      # Ship (ship.hits, ship.position)                           # корабли
-        self.hits = set()    # множество {(x,y), (x,y)}                                  # удар
-
-    def print_board(self):
-        print("  | 1 | 2 | 3 | 4 | 5 | 6 |")
-        for i, row in enumerate(self.board):
-            print(f'{i+1} | {" | ".join(row)} |')
+        self.board = [['0' for _ in range(6)] for _ in range(6)]
+        self.ships = []
+        self.hits = set()
 
     def is_valid_position(self, x, y):
         if not (0 <= x < 6 and 0 <= y < 6):
             return False
 
-        # for hit in self.hits:
-        #     if abs(x - hit[0]) <= 1 and abs(y - hit[1]) <= 1:
-        #         return False
         for hit in self.hits:
             if x == hit[0] and y == hit[1]:
                 return False
@@ -51,29 +50,24 @@ class Board:
         return True
 
     def is_hit(self, x, y):
-        self.hits.add((x, y))  # добавить выстрел в Board.hits (x,y)
+        self.hits.add((x, y))
         for ship in self.ships:
-            # если x,y в корабль.позиция есть
-            if (x, y) in ship.positions:   # если в ships.ship.positon массиве есть (x,y) - координаты
-                                           # positions может иметь вид {(2, 2), (2,3)} или [(2, 2), (2,3)] !
-                ship.hits.append((x, y))     # добавить выстрел (x,y) в ship.hits
-                # self.hits.add((x, y))        # добавить выстрел в Board.hits (x,y)
-                self.board[y][x] = 'X'       # нарисовать 'X' на координате Board.board [y][x]
-                return True                  # вернуть тру
-        # нарисовать на доске 'T'
-        self.board[y][x] = 'T'           # Board.board [y][x] = 'T'
-        return False                     # вернуть фолс
+            if (x, y) in ship.positions:
+                ship.hits.append((x, y))
+                self.board[y][x] = coloredString('RED', 'X')
+                return True
+        self.board[y][x] = coloredString('YELLOW', 'T')
+        return False
 
-# ПРОВЕРЕНО
 def get_player_move(board):
     while True:
         try:
-            x = int(input(coloredString("BLUE", "Введите номер столбца (1-6): "))) - 1
+            x = int(input("\n" + coloredString("BLUE", "Введите номер столбца (1-6): "))) - 1
             y = int(input(coloredString("BLUE", "Введите номер строки (1-6): "))) - 1
             if (x, y) in board.hits:
-                raise ValueError(coloredString("YELLOW", "Вы уже стреляли в эту клетку!"))
+                raise ValueError("\n" + coloredString("YELLOW", "Вы уже стреляли в эту клетку!"))
             if not board.is_valid_position(x, y):
-                raise ValueError(coloredString("YELLOW", "Неверный ход!"))
+                raise ValueError("\n" + coloredString("YELLOW", "Неверный ход!"))
             return x, y
         except ValueError as e:
             print(e)
@@ -106,6 +100,15 @@ def create_random_ships():
 
     return ships
 
+def printBoards(player_board, computer_board):
+    print("")
+    print(coloredString('DARK', "       Доска противника     "), "      ", coloredString('DARK', "         Ваша Доска         "))
+    print(coloredString('BLACK', "   | 1 | 2 | 3 | 4 | 5 | 6 |"), "       " + coloredString('BLACK', "   | 1 | 2 | 3 | 4 | 5 | 6 |"))
+    for i, row in enumerate(player_board.board):
+        print(f'{coloredString('BLACK', " " + str(i + 1) + " " )}{'| '}{" | ".join(computer_board.board[i])}{' |'}        {coloredString('BLACK', " " + str(i + 1) + " ") }{'| '}{" | ".join(row)}{' |'}')
+
+    print("")
+
 def play_game():
     player_board = Board()
     computer_board = Board()
@@ -114,22 +117,17 @@ def play_game():
     computer_board.ships = create_random_ships()
 
     while True:
-        print("Ваша доска:")
-        player_board.print_board()
-        print()
-        print("Доска компьютера:")
-        computer_board.print_board()
-        print()
+        printBoards(player_board, computer_board)
 
         player_x, player_y = get_player_move(computer_board)
 
         if computer_board.is_hit(player_x, player_y):
-            print(coloredString("GREEN", "Вы попали по кораблю противника!"))
+            print("\n", coloredString("GREEN", "Вы попали по кораблю противника!"))
             if all(ship.is_sunk() for ship in computer_board.ships):
-                print(coloredString("GREEN", "Вы выиграли!"))
+                print("\n", coloredString("GREEN", "Вы выиграли!"))
                 break
         else:
-            print(coloredString("YELLOW", "Вы промахнулись!"))
+            print("\n",coloredString("YELLOW", "Вы промахнулись!"))
 
 
         computer_x, computer_y = get_computer_move(player_board)
@@ -137,14 +135,15 @@ def play_game():
         time.sleep(1)
 
         if player_board.is_hit(computer_x, computer_y):
-            print(coloredString("RED", "Компьютер попал по вашему кораблю!"))
+            print("\n", coloredString("RED", "Компьютер попал по вашему кораблю!"))
             if all(ship.is_sunk() for ship in player_board.ships):
-                print(coloredString("RED", "Компьютер выиграл!"))
+                print("\n", coloredString("RED", "Компьютер выиграл!"))
                 break
         else:
-            print(coloredString("GREEN", "Компьютер промахнулся!"))
+            print("\n",coloredString("GREEN", "Компьютер промахнулся!"))
 
         time.sleep(2)
+
 
 
 play_game()
--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("\u001b[31m awdwadwad")
+board = [['O' for _ in range(6)] for _ in range(6)]
+board2 = [['1' for _ in range(6)] for _ in range(6)]
+
+print("       Доска противника                     Ваша Доска")
+print("  | 1 | 2 | 3 | 4 | 5 | 6 |          | 1 | 2 | 3 | 4 | 5 | 6 |")
+for i, row in enumerate(board):
+    print(f'{i+1} | {" | ".join(board[i])} |        {i+1} | {" | ".join(board2[i])} |')
+
